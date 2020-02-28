93cb27acc82cf0352410426b114a1f5a9e1cc559
qudcks0703 <qudcks0703@naver.com>
2020-02-28 16:16:07 +0900

    python0228 study

---
+++ python0228/test01.py
@@ -0,0 +1,44 @@
+'''
+단축키
+실행
+ctrl+shift +f10
+폴더 새파일 생성
+ctrl+alt + insert
+복제:
+ctrl+d
+위아래방향
+alt+shift+위아래
+삭제
+CTRL+Y , SHIFT+D
+화면전환
+CTRL+e
+뛰어가기
+SHIFT+ENTER
+'''
+'''
+print("------------",end="")
+# print안에 , 적으면 띄어쓰기
+# 문자 숫자 더하기 에러
+# \t \n
+print("10",10)
+print("------------")
+print("zz")X
+print("helo")
+'''
+# %d %f %c %s
+'''
+print("%d살이다야 " %10)
+num=100
+print("%.2f" % num)
+print("내이름은 %s이고,시력은 %f이다. " % ("ㅋㅋ",20.0))
+'''
+print("하이룽 내나이는 {} 안눙 {}".format("파이리",10))
+num=10
+print("다운로드가 %d%% 진행중입니다" %num)
+num=20
+print("다운로드가 %d%% 진행중입니다" %num)
+
+
+
+
+
---
+++ python0228/test02.py
@@ -0,0 +1,44 @@
+
+'''
+파이썬2  int,long
+     3  int
+        float
+        bool True/False
+        str
+        list
+        tuple
+        dict
+        set
+
+#변수 varible
+    데이터타입을 기술하지않음
+    type() 데이터타입을 돌려줌
+    num != Num
+'''
+'''
+# num=10
+# num2=10.0
+# print(num)
+# print(type(num))
+# print(type(num2))
+# print(id(num))
+# print(id(num2))
+'''
+'''
+name=input("이름을 입력하세요.")
+print(name)
+'''
+# float("3.14") #문자->실수
+# str(3) #정수/실수->문자
+# int("10") #문자->정수
+#
+# age=int(input("나이 입력:"))
+# print("당신의 내년 나이는 %d 살입니다." %(age+1))
+#
+# kor=int(input("국어 점수="))
+# eng=int(input("영어 점수="))
+# math=int(input("수학 점수="))
+#
+# print("총점=%d \n평균=%f" %(kor+eng+math,(kor+eng+math)/3))
+# print("총점={} \n 평균={}".format((kor+eng+math),(kor+eng+math)/3))
+
